3e3f25668efd6261a84332d446f69daad2a2b6b7
wesleyclzns <calazanscafe@outlook.com>
2021-11-30 20:18:35 -0300

    esta tudo ok, mas preciso testar se esta funcionando

--- asn/asn/spiders/anos.py
+++ asn/asn/spiders/anos.py
@@ -8,8 +8,19 @@ class AnsWikiSpider(scrapy.Spider):
     
     def parse(self, response):
         
-        #Pega os anos 
+        ### DECLARAÇÃO DE LISTAS USADAS NO CODIGO
+        #Lista dos anos
         anos=[]
+        #listas de link dos anos
+        urlAnos = []
+        #lista de link dos casos
+        urlCasos = []
+        #Lista de numero das paginas
+        paginas = []
+        #Lista de link das paginas
+        urlPaginas = []
+
+        #Pega os anos 
         anos1 = int(response.css('font+ a ::text').get())
         anos2 = response.css('a+ a ::text').getall()
         anos3 = response.css('br+ a ::text').getall()
@@ -29,12 +40,6 @@ class AnsWikiSpider(scrapy.Spider):
         urlPagina = '&sorteer=datekey&page='
         urlCasosAno = urlHome + urlBaseAno
 
-        #listas de link dos anos
-        urlAnos = []
-        #lista de link dos casos
-        urlCasos = []
-
-
         #Pega o numero do ano e coloca no link "https://aviation-safety.net/wikibase/dblist.php?Year=" e faz uma lista
         for ano in anos:
             urlAno = response.url + urlBaseAno + str(ano)
@@ -46,52 +51,69 @@ class AnsWikiSpider(scrapy.Spider):
                 
                 #Pega o link de cada caso/incidente
                 casos = response.css('.nobr a ::attr(href)').getall()
-
-                    for caso in casos:
-                        urlCaso = (urlPrincipal + str(caso))
-                        urlCasos.append(urlCaso)
-
+                
+                #Pega o link de cada pagina no ano
                 paginas= response.css('.pagenumbers a ::attr(href)').getall()
+    
+                #Trata o link do caso
+                for caso in casos:
+                    urlCaso = (urlPrincipal + str(caso))
+                    urlCasos.append(urlCaso)
 
-                    for pagina in paginas:
-                        pagina = pagina[10:-1]
-                        
+                #Trata o link da pagina 
+                for pagina in paginas:
+                    pagina = pagina[10:]
+                    urldapagina = (response.url + pagina)
+                    urlPaginas.append(urldapagina)
 
-                        for linkCaso in urlCasos:
-                            start_urls = [linkAno]
+                #Se tiver paginas extras no ano, ele entra e pega o link do caso
+                if paginas:
+                    for urlpg in urlPaginas:
+                        scrapy.Request(urlpg)
 
-                            yield{
+                        casosDasOutrasPaginas = response.css('.nobr a ::attr(href)').getall()
 
-                                'ano': ano
+                        #Trata o link do caso e coloca na lista de url dos casos
+                        for caso in casosDasOutrasPaginas:
+                            urlDoCaso = urlPrincipal + str(caso)
+                            urlCasos.append(urlDoCaso)
 
-                                #ID, data e horario
-                                'idWiki': response.css('.pagetitle ::text').get(),
-                                'dataOcorencia': response.css('.caption+ .caption ::text').get(),
-                                'horarioOcorencia': response.css('tr:nth-child(2) .desc ::text').get(),
+                    #Entra em cada link na lista de casos
+                    for linkCaso in urlCasos:
+                        start_urls = [linkAno]
 
-                                #Informações sobre o modelo
-                                'modelo': response.css('tr:nth-child(3) .desc ::text').get(),
-                                'modeloLink': response.css('tr:nth-child(3) .desc ::attr(href)').get(),
-                                'modeloSilueta': response.css('img').attrib['src'],
-                                'registroAeronave': response.css('tr:nth-child(5) .desc ::text').get(),
-                                'msn': response.css('tr:nth-child(6) .desc ::text').get(),
-                                'proprietarioOperador': response.css('tr:nth-child(4) .desc ::text').get(),
+                        #Solicita os dados do caso
+                        yield{
+                            'ano': ano
 
-                                #Informações sobre o incidente
-                                'fatalidadesOcupantes': response.css('tr:nth-child(7) .desc ::text').get(),
-                                'outrasFatalidades': response.css('tr:nth-child(8) .desc ::text').get(),
-                                'danoAeronave': response.css('tr:nth-child(9) .desc ::text').get(),
-                                'categoriaIncidente': response.css('tr:nth-child(10) .desc ::text').get(),
-                                'faseVoo': response.css('tr:nth-child(12) .desc ::text').get(),
-                                'naturezaVoo': response.css('tr:nth-child(13) .desc ::text').get(),
+                            #ID, data e horario
+                            'idWiki': response.css('.pagetitle ::text').get(),
+                            'dataOcorencia': response.css('.caption+ .caption ::text').get(),
+                            'horarioOcorencia': response.css('tr:nth-child(2) .desc ::text').get(),
 
-                                #Informações sobre a localização do incidente
-                                'localizacao': response.css('tr:nth-child(11) .desc ::text').get(),
-                                'pais': response.css('img+ a ::text').get(),
-                                'coordenadas': response.css('br+ iframe').attrib['src'][53:],
-                                'aeroportoOrigem': response.css('tr:nth-child(14) .desc ::text').get(),
-                                'aeroportoDestino': response.css('tr:nth-child(15) .desc ::text').get(),
-                                'narativa': response.css('br+ span ::text').get(),
+                            #Informações sobre o modelo
+                            'modelo': response.css('tr:nth-child(3) .desc ::text').get(),
+                            'modeloLink': response.css('tr:nth-child(3) .desc ::attr(href)').get(),
+                            'modeloSilueta': response.css('img').attrib['src'],
+                            'registroAeronave': response.css('tr:nth-child(5) .desc ::text').get(),
+                            'msn': response.css('tr:nth-child(6) .desc ::text').get(),
+                            'proprietarioOperador': response.css('tr:nth-child(4) .desc ::text').get(),
+
+                            #Informações sobre o incidente
+                            'fatalidadesOcupantes': response.css('tr:nth-child(7) .desc ::text').get(),
+                            'outrasFatalidades': response.css('tr:nth-child(8) .desc ::text').get(),
+                            'danoAeronave': response.css('tr:nth-child(9) .desc ::text').get(),
+                            'categoriaIncidente': response.css('tr:nth-child(10) .desc ::text').get(),
+                            'faseVoo': response.css('tr:nth-child(12) .desc ::text').get(),
+                            'naturezaVoo': response.css('tr:nth-child(13) .desc ::text').get(),
+
+                            #Informações sobre a localização do incidente
+                            'localizacao': response.css('tr:nth-child(11) .desc ::text').get(),
+                            'pais': response.css('img+ a ::text').get(),
+                            'coordenadas': response.css('br+ iframe').attrib['src'][53:],
+                            'aeroportoOrigem': response.css('tr:nth-child(14) .desc ::text').get(),
+                            'aeroportoDestino': response.css('tr:nth-child(15) .desc ::text').get(),
+                            'narativa': response.css('br+ span ::text').get(),
                             }
 
 
